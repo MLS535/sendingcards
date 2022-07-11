bf707444b34a730387915289876e2ecfc02fb9e8
Maite <mladariac2t@gmail.com>
2022-07-11 20:37:52 +0200

    Push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import {useState} from "react";
+
+const App = () => {
+    const [inputPeso, setInputPeso] = useState("");
+
+    const onInputPesoChange = ({target}) => {
+      setInputPeso(target.value);
+    }
+
+    const checkInputPeso = (inputPeso) => {
+        if (inputPeso > 60) {
+           return  `El peso es de ${inputPeso} y por tanto el precio del envío es 10€`;
+        }else if (inputPeso <= 60 && inputPeso >= 25) {
+            return  `El peso es de ${inputPeso} y por tanto el precio del envío es 6€`;
+        }
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <form
+         // onSubmit={(event) => onSubmit(event)}
+     >
+       <input type={"number"} name={"peso"}
+              value={inputPeso}
+              onChange={  (event) => onInputPesoChange(event)}
+       />
+     </form>
+        <h3>{checkInputPeso(inputPeso)}</h3>
     </div>
   );
 }
